6cf567342abdc50a0d0b67dfd1dc54e4393c1c68
erwan.boehm@gmail.com <boehm_e@etna-alternance.net>
2019-08-20 11:13:31 +0200

    update to look more like spacy

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# google_nlp_to_spacy
+# GOOGLE NLP TO SPACY
 
 A wrapper to make google cloud natural language API return elements structured like spacy
 
 ### Context
-I was working on a NLP project and began to create algorithmes with spacy classes (mostly the Token class).
-For some reason (mostly scalability, and performances) I decided to switch to google cloud language, which acchieve the current state of the art results on 11 NLP tasks ([google blog](https://ai.googleblog.com/2018/11/open-sourcing-bert-state-of-art-pre.html))
+I was working on a NLP project and began to create algorithmes with spacy classes.
+For some reason (mostly scalability, and performances) I decided to switch to google cloud language, which achieve the current state of the art results on 11 NLP tasks ([google blog](https://ai.googleblog.com/2018/11/open-sourcing-bert-state-of-art-pre.html))
 
 ### Goal
-The goal of this project is to provide a simple transition from Spacy to Google Cloud NLP by providing wrappers to Google NLP that matches Spacy documentation.
-So (ideally) we only have to change a few line to our code and all would be working.
+The goal of this project is to provide a simple transition from Spacy to Google Cloud NLP by providing wrappers to Google NLP that matches Spacy's documentation.
+So (ideally) we only have to change a few line to our code and all is working.
 
 ### Prerequisites
 
@@ -31,12 +31,13 @@ To use this module do as follow :
 
 - use in your project
     ```python
-    from google_spacy import GoogleSpacy
+    #setup
     gspacy = GoogleSpacy()
-    sentences, tokens = gspacy.nlp("The lion is walking.")
+    gspacy.set_language('fr')
+    gnlp = gspacy.nlp
 
-
-    for token in tokens:
+    doc = gnlp("The lion is walking.")
+    for token in doc:
       print( token.text, token.lemma_, token.pos_, token.dep_ )
 
     # The       The     DET    DET
@@ -44,14 +45,28 @@ To use this module do as follow :
     # is        be      VERB   AUX
     # walking   walk    VERB   ROOT
 
-    sentences, tokens = gspacy.nlp("The lion is walking. It is in the forest.")
-    print(sentences)
+
+    doc = gnlp("The lion is walking. It is in the forest.")
+    print(doc.sentences)
+    # [ ["The", "lion", "is", "walking", "."], ["It", "is", "in", "the", "forest", "."] ]
     # each is of type GSToken, so you can access its dep, pos, lemma ...
-    # [ ["The", "lion", "is", "walking", "."], ["It", "is", "in", "the", "forest."] ]
 
     ```
 
+- serialization and deserialization
+    ```python
+    # you can call gnlp with text or serialized bytes (from database)
 
+    # serialize
+    doc = gnlp("Avec la mer du Nord pour dernier terrain vague")
+
+    serializedDoc = doc.serialize()
+    #> b'\n2\n0\n.Avec la mer du Nord pour dernier terrain vague\x12\x18\n\x06\n\x04Avec\x12\x04\x08\x02H\x02\x1a\x02\x106"\x04Avec\x12\x1c\n\x06\n\x02la\x10\x05\x12\x08\x08\x05(\x018\x01H\x02\x1a\x04\x08\x02\x10\x10"\x02le\x12\x1c\n\x07\n\x03mer\x10\x08\x12\x08\x08\x06(\x018\x01H\x02\x1a\x02\x10$"\x03mer\x12\x1c\n\x06\n\x02du\x10\x0c\x12\x08\x08\x02(\x028\x01H\x02\x1a\x04\x08\x02\x10+"\x02du\x12 \n\x08\n\x04Nord\x10\x0f\x12\x08\x08\x06(\x028\x01H\x01\x1a\x04\x08\x03\x10$"\x04Nord\x12\x1a\n\x08\n\x04pour\x10\x14\x12\x04\x08\x02H\x02\x1a\x02\x10+"\x04pour\x12&\n\x0b\n\x07dernier\x10\x19\x12\x08\x08\x01(\x028\x01H\x02\x1a\x04\x08\x07\x10\x05"\x07dernier\x12&\n\x0b\n\x07terrain\x10!\x12\x08\x08\x06(\x028\x01H\x02\x1a\x04\x08\x05\x10$"\x07terrain\x12"\n\t\n\x05vague\x10)\x12\x08\x08\x06(\x028\x01H\x02\x1a\x04\x08\x07\x10\x1a"\x05vague\x1a\x02fr'
+
+    deserializedDoc = gnlp(serializedDoc)
+
+    # here doc and deserializedDoc are the same
+    ```
 ## References
 
 - ### GSToken
--- google_spacy.py
+++ google_spacy.py
@@ -2,50 +2,66 @@
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import re
 
-pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN',
-           'NUM', 'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
-dep_tag = ('UNKNOWN', 'abbrev', 'acomp', 'advcl', 'advmod', 'amod', 'appos', 'attr', 'aux', 'auxpass', 'cc', 'ccomp', 'conj', 'csubj', 'csubjpass', 'dep', 'det', 'discourse', 'dobj', 'expl', 'goeswith', 'iobj', 'mark', 'mwe', 'mwv', 'neg', 'nn', 'npadvmod', 'nsubj', 'nsubjpass', 'num', 'number', 'p', 'parataxis', 'partmod', 'pcomp', 'pobj', 'poss', 'postneg', 'precomp', 'preconj', 'predet',
-           'pref', 'prep', 'pronl', 'prt', 'ps', 'quantmod', 'rcmod', 'rcmodrel', 'rdrop', 'ref', 'remnant', 'reparandum', 'root', 'snum', 'suff', 'tmod', 'topic', 'vmod', 'vocative', 'xcomp', 'suffix', 'title', 'advphmod', 'auxcaus', 'auxvv', 'dtmod', 'foreign', 'kw', 'list', 'nomc', 'nomcsubj', 'nomcsubjpass', 'numc', 'cop', 'dislocated', 'asp', 'gmod', 'gobj', 'infmod', 'mes', 'ncomp')
-
-
-class GSToken(object):
-    def __init__(self, token, index):
-        # self.token = self.build_token(token, i)
-        self.text = token.text.content
-        self.lower_ = token.text.content.lower()
-        self.shape = "".join(["X" if x.isupper() else "x" for x in self.text])
-        self.lemma_ = token.lemma
-        self.pos_ = pos_tag[token.part_of_speech.tag]
-        self.dep_ = dep_tag[token.dependency_edge.label]
-        self.head = token.dependency_edge.head_token_index
-        self.head_ = token.dependency_edge.head_token_index
-        self.is_lower = self.text.islower()
-        self.is_upper = self.text.isupper()
-        self.is_title = self.text.istitle()
-        self.is_space = self.text.isspace()
-        self.i = index
-        self.idx = token.text.begin_offset  # pos of first letter of token
-
-    def __repr__(self):
-        return repr(self.text)
-
-    def make_references(self, tokens):
-        self.lefts = [left_token for left_token in tokens if self.i == tokens[left_token.head_].i and left_token.i < self.i]
-        self.rights = [right_token for right_token in tokens if self.i == tokens[right_token.head_].i and right_token.i > self.i]
-        self.head = tokens[self.head]
+pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
+dep_tag = ('UNKNOWN', 'abbrev', 'acomp', 'advcl', 'advmod', 'amod', 'appos', 'attr', 'aux', 'auxpass', 'cc', 'ccomp', 'conj', 'csubj', 'csubjpass', 'dep', 'det', 'discourse', 'dobj', 'expl', 'goeswith', 'iobj', 'mark', 'mwe', 'mwv', 'neg', 'nn', 'npadvmod', 'nsubj', 'nsubjpass', 'num', 'number', 'p', 'parataxis', 'partmod', 'pcomp', 'pobj', 'poss', 'postneg', 'precomp', 'preconj', 'predet', 'pref', 'prep', 'pronl', 'prt', 'ps', 'quantmod', 'rcmod', 'rcmodrel', 'rdrop', 'ref', 'remnant', 'reparandum', 'root', 'snum', 'suff', 'tmod', 'topic', 'vmod', 'vocative', 'xcomp', 'suffix', 'title', 'advphmod', 'auxcaus', 'auxvv', 'dtmod', 'foreign', 'kw', 'list', 'nomc', 'nomcsubj', 'nomcsubjpass', 'numc', 'cop', 'dislocated', 'asp', 'gmod', 'gobj', 'infmod', 'mes', 'ncomp')
+gender_tag = ('GENDER_UNKNOWN', 'FEMININE', 'MASCULINE', 'NEUTER')
+person_tag = ('PERSON_UNKNOWN', 'FIRST', 'SECOND', 'THIRD', 'REFLEXIVE_PERSON')
+number_tag = ('NUMBER_UNKNOWN', 'SINGULAR', 'PLURAL', 'DUAL')
 
 
-class GoogleSpacy(object):
-    def __init__(self):
-        self.lang = 'en'
-        self.GSTokens = []
+# helpers
+def to_string(txt):
+    punctRegex = re.compile('\s(?=[\.\?!,])', flags=re.IGNORECASE)
+    apostrophRegex = re.compile("(?<=(l'|s'|m'|n'|t'))\s", flags=re.IGNORECASE)
+    txt = punctRegex.sub('', txt)
+    txt = apostrophRegex.sub('', txt)
+    return txt
+
+
+class Nlp(object):
+    """docstring for Nlp."""
+
+    def __init__(self, ret):
+        super(Nlp, self).__init__()
+
+        self.ret = ret
+        self.doc = self.process_tokens(self.ret.tokens)
+        self.sentences = self.process_sentences(self.doc, self.ret.sentences)
+
+    def __getitem__(self, index):
+        return self.doc[index]
 
     def __str__(self):
-        return 'Fubar'
+        # txt = " ".join([tok.text for tok in self.doc])
+        return self.to_string()
 
-    def set_language(self, lang):
-        self.lang = lang
+    def serialize(self):
+        return self.ret.SerializeToString()
+
+    def to_string(self):
+        txt = " ".join([tok.text for tok in self.doc])
+        return to_string(txt)
+
+    def find_children_direct(self, head):
+        return [tok for tok in self.doc if tok if tok.head.i == head.i]
+
+    def find_children_tree(self, head):
+        children_final = []  # liste contenant toute l'arborescence des enfants d'un token
+        children_direct = self.find_children_direct(head)  # liste contenant les enfants direct d'un token
+        children_final.append(children_direct)  # on ajoute au children_final, les enfants direct du token recherche
+        while children_direct:  # tant qu'on decouvre de nouveaux enfants a un token
+            new_children = []
+            for c in children_direct:
+                new_children += self.find_children_direct(c)
+
+            if len(new_children) > 0:
+                children_final.append(new_children)
+            children_direct = new_children
+        flatten = [item for sublist in children_final for item in sublist]  # flatten list
+        flatten.sort(key=lambda x: x.i)
+        return flatten
 
     def process_tokens(self, tokens):
         self.GSTokens = []
@@ -68,18 +84,93 @@ class GoogleSpacy(object):
 
         return sentences
 
+
+class GSToken(object):
+    def __init__(self, token, index):
+        # self.token = self.build_token(token, i)
+        self.text = token.text.content
+        self.lower_ = token.text.content.lower()
+        self.shape = "".join(["X" if x.isupper() else "x" for x in self.text])
+        self.lemma_ = token.lemma
+        self.pos_ = pos_tag[token.part_of_speech.tag]
+        self.dep_ = dep_tag[token.dependency_edge.label]
+        self.head = token.dependency_edge.head_token_index
+        self.gender = gender_tag[token.part_of_speech.gender]
+        self.person = person_tag[token.part_of_speech.person]
+        self.number = number_tag[token.part_of_speech.number]
+        self.head_ = token.dependency_edge.head_token_index
+        self.is_lower = self.text.islower()
+        self.is_upper = self.text.isupper()
+        self.is_title = self.text.istitle()
+        self.is_space = self.text.isspace()
+        self.i = index
+        self.idx = token.text.begin_offset  # pos of first letter of token
+
+    def __repr__(self):
+        return repr(self.text)
+
+    def make_references(self, tokens):
+        self.lefts = [left_token for left_token in tokens if self.i == tokens[left_token.head_].i and left_token.i < self.i]
+        self.rights = [right_token for right_token in tokens if self.i == tokens[right_token.head_].i and right_token.i > self.i]
+        self.head = tokens[self.head]
+        # print(self.text, self.lefts, self.rights)
+
+
+class GoogleSpacy(object):
+    def __init__(self):
+        self.lang = 'en'
+        self.GSTokens = []
+
+    def __str__(self):
+        return 'Fubar'
+
+    def set_language(self, lang):
+        self.lang = lang
+
+    def sort_by_index(self, tokList):
+        lst = tokList
+        lst.sort(key=lambda x: x.i)
+        return lst
+
+    def doc_to_string(self, doc):
+        txt = " ".join([tok.text for tok in doc])
+        return to_string(txt)
+
+    def find_children_direct(self, doc, head):
+        return [tok for tok in doc if tok if tok.head.i == head.i]
+
+    def find_children_tree(self, doc, head):
+        children_final = []  # liste contenant toute l'arborescence des enfants d'un token
+        children_direct = self.find_children_direct(doc, head)  # liste contenant les enfants direct d'un token
+        children_final.append(children_direct)  # on ajoute au children_final, les enfants direct du token recherche
+        while children_direct:  # tant qu'on decouvre de nouveaux enfants a un token
+            new_children = []
+            for c in children_direct:
+                new_children += self.find_children_direct(doc, c)
+
+            if len(new_children) > 0:
+                children_final.append(new_children)
+            children_direct = new_children
+        flatten = [item for sublist in children_final for item in sublist]  # flatten list
+        flatten.sort(key=lambda x: x.i)
+        return flatten
+
+    def correct_punct(self, txt):
+        return to_string(txt)
+
     def nlp(self, text):
-        # Instantiates a client
-        client = language.LanguageServiceClient()
+        if isinstance(text, str):
+            # Instantiates a client
+            client = language.LanguageServiceClient()
+            # Instantiates a plain text document.
+            document = types.Document(
+                content=text,
+                language=self.lang,
+                type=enums.Document.Type.PLAIN_TEXT)
 
-        # Instantiates a plain text document.
-        document = types.Document(
-            content=text,
-            language=self.lang,
-            type=enums.Document.Type.PLAIN_TEXT)
+            # Detects syntax in the document. You can also analyze HTML with:
+            ret = client.analyze_syntax(document, enums.EncodingType.UTF32)
+            return Nlp(ret)
 
-        # Detects syntax in the document. You can also analyze HTML with:
-        ret = client.analyze_syntax(document, enums.EncodingType.UTF32)
-        doc = self.process_tokens(ret.tokens)
-        sentences = self.process_sentences(doc, ret.sentences)
-        return [sentences, doc]
+        elif isinstance(text, bytes):
+            return Nlp(types.AnalyzeSyntaxResponse().FromString(text))
